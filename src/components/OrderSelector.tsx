3324b0f981a63fd695a64ac96f74a9eb8d7172dd
Leo <leonel.m.cata03@gmail.com>
2023-10-24 16:21:39 -0300

    minor changes to the selector

--- src/components/OrderSelector.tsx
+++ src/components/OrderSelector.tsx
@@ -16,22 +16,8 @@ const OrderSelector = ({
 }) => {
   // change ordering state
   const handleChange = (event: SelectChangeEvent) => {
-    console.log("asd");
     setOrdering(event.target.value);
   };
-
-  // // if pressing on the same element, add - to reverse the order
-  // const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-  //   console.log("dsa");
-  //   // tells typescript to take this as an HTML li Element
-  //   const target = event.target as HTMLLIElement;
-
-  //   if (ordering === target.innerText) {
-  //     setOrdering(`-${ordering}`);
-  //   } else {
-  //     setOrdering(target.innerText);
-  //   }
-  // };
 
   return (
     <FormControl className="max-w-[15rem] bg-custom-cards">
@@ -48,23 +34,10 @@ const OrderSelector = ({
         value={ordering}
         onChange={handleChange}
         className="capitalize transition-transform"
-        // onClick={handleClick}
-        // startAdornment={
-        //   <ArrowUpwardIcon
-        //     className={`${
-        //       ordering.includes("-") ? "rotate-180 " : " "
-        //     } transition-transform duration-300`}
-        //     fontSize="small"
-        //   />
-        // }
       >
         <MenuItem value="relevance">Relevance</MenuItem>
         <MenuItem value="name">Name</MenuItem>
-        <MenuItem value="-released">New</MenuItem>
-        {/* <MenuItem value="-added">Added</MenuItem> */}
-        {/* <MenuItem value="created">Created</MenuItem> */}
-        {/* <MenuItem value="-updated">Updated</MenuItem> */}
-        {/* <MenuItem value="-rating">Rating</MenuItem> */}
+        <MenuItem value="-released">Release Date</MenuItem>
         <MenuItem value="-metacritic">Metacritic</MenuItem>
       </Select>
     </FormControl>
