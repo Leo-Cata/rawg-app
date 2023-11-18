136a45482310e5332ff21426b5bb4c02b00e176d
Leo <leonel.m.cata03@gmail.com>
2023-11-18 18:31:20 -0300

    minor css change to NoGamesBacklogged

--- src/components/NoGamesBacklogged.tsx
+++ src/components/NoGamesBacklogged.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, Typography } from "@mui/material";
 const NoGamesBacklogged = () => {
   return (
     <Card className="max-w-[500px] rounded-2xl">
-      <CardContent className="m-10 flex items-center justify-center">
+      <CardContent className="my-5 flex items-center justify-center">
         <Typography variant="h6">
           You've cleared your backlog 🤯! ...or you haven't started it yet 🤨
         </Typography>
