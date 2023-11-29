196158af3353fdc13285d5a064318de928273966
Leo <leonel.m.cata03@gmail.com>
2023-11-29 17:04:56 -0300

    side panel skeletons reworked

--- src/components/Skeletons/SidePanelGenresSkeleton.tsx
+++ src/components/Skeletons/SidePanelGenresSkeleton.tsx
@@ -1,19 +1,10 @@
 import { Skeleton } from "@mui/material";
 
 const SidePanelGenresSkeleton = () => {
-  return (
-    <div className="mx-4 mb-4 flex flex-col lg:block lg:max-w-[200px]">
-      <Skeleton className="h-7 w-full transform-none self-center" />
+  // width must be 179 because for some reason that makes it match the width of the rendered side panels
 
-      <div className="hidden h-fit w-full overflow-y-auto lg:block lg:max-w-[200px]">
-        {Array.from({ length: 19 }, (_, index) => (
-          <Skeleton
-            key={index * 123}
-            className="m-2 h-12 min-w-[5rem] transform-none rounded-md"
-          />
-        ))}
-      </div>
-    </div>
+  return (
+    <Skeleton className="mx-4 mb-2 h-7 transform-none rounded-md lg:mx-1 lg:w-[179px]" />
   );
 };
 
